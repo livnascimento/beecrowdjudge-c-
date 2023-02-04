ed1a22b954427f2bc161f0979326d461fceda9d3
Lívia Nascimento <liviapnd@gmail.com>
2023-02-04 18:53:17 -0300

    Final Beecrowd1001

---
+++ iniciante/Beecrowd1001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+
+        int a = Int32.Parse( System.Console.ReadLine().Trim());
+        int b = Int32.Parse( System.Console.ReadLine().Trim());
+ 
+        int X = a + b;
+ 
+        Console.Write("X = {0}\n", X);
+    }
+}
